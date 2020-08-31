734b9daf52230d24a613ab5542dcc72cf88c75bf
MuhammadIchsan21 <ichsanteratai@gmail.com>
2020-08-31 20:37:05 +0800

    mengintegrasikan halaman detail ke laravel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,9 +9,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>NOMADS Admin</title>
 
-    @include('includes.style')
+    @include('includes.admin.style')
 
 </head>
 
@@ -19,23 +19,22 @@
 
     <!-- Page Wrapper -->
     <div id="wrapper">
-        @include('includes.sidebar')
+        @include('includes.admin.sidebar')
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Main Content -->
             <div id="content">
-                @include('includes.navbar')
+                @include('includes.admin.navbar')
                 @yield('content')
             </div>
             <!-- End of Main Content -->
-            @include('includes.footer')
+            @include('includes.admin.footer')
         </div>
         <!-- End of Content Wrapper -->
     </div>
     <!-- End of Page Wrapper -->
-
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
@@ -61,7 +60,7 @@
         </div>
     </div>
 
-    @include('includes.script')
+    @include('includes.admin.script')
 
 </body>
 
